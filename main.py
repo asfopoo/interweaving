fb15eceb711bb16fef5512b541056232276a069d
“[Edward <edwardnardo@MacBook-Air.local>
2021-11-21 16:59:41 -0500

    graph runs

--- main.py
+++ main.py
@@ -22,10 +22,12 @@ def is_interleave_recursive(s, x, y, x_orig, y_orig):
         return is_interleave_recursive(s[1:], x, y[1:], x_orig, y_orig)
 """
 
+
 # dynamic programming solution
-def is_interleave(s, x, y):
+def is_interleave(s, x, y, count):
     # too short to determine
     if len(x) + len(y) > len(s):
+        count += 1
         return False
     # create list of lists representing a grid of size len(s) X len(s)
     path = [[False] * len(s) for i in range(len(s))]
@@ -35,16 +37,19 @@ def is_interleave(s, x, y):
 
             # check the first element seperately
             if i == 0 and j == 0:
+                count += 1
                 if s[0] == x[0] or s[0] == y[0]:
                     path[0][0] = True
                     continue
 
             # ensure we dont go out of bounds
             if i + j > len(s)-1:
+                count += 1
                 continue
 
             # is the cell above true and does s match x
             if i > 0 and path[i - 1][j] and s[i + j] == x[i % len(x)]:
+                count += 1
                 path[i][j] = True
                 # return true if we've matched all of s
                 if i + j == len(s) - 1:
@@ -52,16 +57,19 @@ def is_interleave(s, x, y):
             # is the cell to the left true and does s match x
             elif j > 0:
                 if path[i][j - 1] and s[i + j] == y[j % len(y)]:
+                    count += 1
                     path[i][j] = True
                     # return true if we've matched all of s
                     if i + j == len(s) - 1:
                         return True
             else:
+                count += 1
                 path[i][j] = False
-    return False
+    return False, count
 
 
 if __name__ == "__main__":
 
-    path = is_interleave('100010101', '101', '0')
+    count = 0
+    path, final_count = is_interleave('100010101', '101', '0', count)
     print(path)
